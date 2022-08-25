f3c9d00aa740a50db94d639d6b34a8bd19a098d7
Szymon Świątek <Szymon.Swiatek@Strauss-Group.pl>
2022-08-25 14:49:31 +0200

    third

--- sensor.js
+++ sensor.js
@@ -6,16 +6,18 @@ class Sensor {
     this.raySpread = Math.PI / 4;
 
     this.rays = [];
+    this.readings = [];
   }
 
   update() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      const rayAngle = lerp(
-        this.raySpread / 2,
-        -this.raySpread / 2,
-        i / (this.rayCount - 1)
-      );
+      const rayAngle =
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
 
       const start = { x: this.car.x, y: this.car.y };
       const end = {
@@ -23,6 +25,17 @@ class Sensor {
         y: this.car.y - Math.cos(rayAngle) * this.rayLength,
       };
       this.rays.push([start, end]);
+    }
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.stroke();
     }
   }
 }
